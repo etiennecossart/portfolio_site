99d80bbd41f468909d923c402025a2bb15e94a74
etiennecossart <41974406+etiennecossart@users.noreply.github.com>
2021-07-24 20:39:06 -0400

    added state for handling project active

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useImperativeHandle, useState } from 'react'
+import GitHubLogo from '../../../Resources/github.jpeg'
 import './Project.css'
 
 function Project (props){
+    const [active, setActive] = useState(false)
+
     return(
-        <div className="project-main">
-            <div className="project-header">
-                <h2>{props.project.name}</h2>
-                <h4>{props.project.date}</h4>
-            </div>
-            <h3>{props.project.stack}</h3>
-            <div className="project-body">
-                <div className="project-body-text">
-                    {<p >{props.project.dscr}</p>}
-                </div>
-                <div className="project-body-text">
-                    { props.project.learnings ? <p><span>Learnings: </span>{props.project.learnings}</p> : null }
-                </div>
-                <div className="project-body-text">
-                    { props.project.planAugmt ? <p><span>Planned Augmentations: </span>{props.project.planAugmt}</p> : null }
-                </div>
-                <div>
-                {props.project.github ? <a href={props.project.github}>GitHub</a> : null}
+        // <div className="project-main">
+        //     <div className="project-header">
+        //         <h2>{props.project.name}</h2>
+        //         <h4>{props.project.date}</h4>
+        //     </div>
+        //     <h3>{props.project.stack}</h3>
+        //     <div className="project-body">
+        //         <div className="project-body-text">
+                    // <p>{props.project.dscr}</p>
+        //         </div>
+        //         <div className="project-body-text">
+        //             { props.project.learnings ? <p><span>Learnings: </span>{props.project.learnings}</p> : null }
+        //         </div>
+        //         <div className="project-body-text">
+        //             { props.project.planAugmt ? <p><span>Planned Augmentations: </span>{props.project.planAugmt}</p> : null }
+        //         </div>
+        //         <div>
+        //         {props.project.github ? <a href={props.project.github}><img src={GitHubLogo} className = "project-github"/></a> : null}
                     
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        <div className="project-main">
+            <div className="project-heading">
+                <div className="container">
+                    <div className="project-header" onClick={() => setActive(!active)}>
+                        <div className="project-header-text">
+                            <div className="project-title-line">
+                                <h2>{props.project.name}</h2>
+                                <h4>{props.project.date}</h4>
+                            </div>
+                            <h3>{props.project.stack}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className={(active == true ? "show " : "") + "project-content"}>
+                    <p>{props.project.dscr}</p>
+                    { props.project.learnings ? <p><span>Learnings: </span>{props.project.learnings}</p> : null }
+                    { props.project.planAugmt ? <p><span>Planned Augmentations: </span>{props.project.planAugmt}</p> : null }
+                    { props.project.github ? <a href={props.project.github}><img src={GitHubLogo} className = "project-github"/></a> : null}
                 </div>
             </div>
         </div>
--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -1,4 +1,4 @@
-import {React, Component }from 'react'
+import {React, Component, useState}from 'react'
 import prjData from './ProjectData'
 import Project from './Project/Project'
 import './ProjectList.css'
@@ -15,8 +15,20 @@ import './ProjectList.css'
 
 
 class ProjectList extends Component{
+    // const [active, setActive] = useState()
+    constructor(){
+        super()
+        this.state = {
+            projectList: prjData
+        }
+        this.handleActive = this.handleActive.bind(this)
+    }
+
+    handleActive(id){
+        console.log('Active: ', id)
+    }
     render() {
-        const projects = prjData.map(prj => <Project key = {prj.id } project={prj} />)
+        const projects = prjData.map(prj => <Project key = {prj.id } project={prj} handleActive={this.handleActive}/>)
         return(
             <div className="project-list-main">
                 {projects}
